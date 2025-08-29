e13d92ff90609d280b9a22e0da0dc415b53f20f6
abhi6004 <abhiradadiya30012004@gmail.com>
2025-08-29 19:02:10 +0530

    lib download

---
+++ Controllers/Account.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureBank_Pro.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult TransactionForm()
+        {
+            return PartialView("MainForm");
+        }
+    }
+}
