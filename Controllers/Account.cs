3a4b23d181206072bcd72b301806e5a2728900de
abhi6004 <abhiradadiya30012004@gmail.com>
2025-08-31 10:46:23 +0530

    Account Controllers Code

--- Controllers/Account.cs
+++ Controllers/Account.cs
@@ -1,12 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SecureBank_Pro.Data;
 
 namespace SecureBank_Pro.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly BankDbContext _context;
+        public AccountController(BankDbContext context) => _context = context;
+
         [HttpGet]
-        public IActionResult TransactionForm()
+        public IActionResult TransactionForm(string email)
         {
+            ViewBag.email = email;
+            return PartialView("MainForm");
+        }
+
+        [HttpPost]
+        public IActionResult Transaction([FromBody] JObject data)
+        {
+            //var amount = data["Amount"]?.ToObject<decimal>();
+            //var recipient = data["RecipientId"]?.ToObject<string?>();
+
+            decimal amount = 0;
+            string recipient = "";
+            string Type = "";
+            string Email = "";
+
+            if (data != null)
+            {
+                if (data["Amount"] != null)
+                {
+                    amount = (decimal)data["Amount"];
+                }
+
+                if (data["RecipientId"] != null)
+                {
+                    recipient = (string)data["RecipientId"];
+                }
+
+                if (data["Type"] != null)
+                {
+                    Type = (string)data["Type"];
+                }
+
+                if (data["Email"] != null)
+                {
+                    Email = (string)data["Email"];
+                }
+
+            }
+
+
+            if (Type == "withdraw")
+            {
+                SecureBank_Pro.Services.Account.WithdrawMoney(_context, amount, Type , Email);
+            }
+            else if (Type == "deposit")
+            {
+                SecureBank_Pro.Services.Account.AddBalance(_context ,amount, Type , Email);
+            }
+            else if (Type == "transfer")
+            {
+                SecureBank_Pro.Services.Account.UserTransfer(_context , amount, Type, Email, recipient);
+            }
+
             return PartialView("MainForm");
         }
     }
---
+++ Services/Account.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureBank_Pro.Data;
+
+namespace SecureBank_Pro.Services
+{
+    public class Account
+    {
+        public static async void WithdrawMoney(BankDbContext context, decimal amount, string type, string email)
+        {
+            var userId = await context.Users.Where(e => e.email == email).Select(e => e.id).FirstOrDefaultAsync();
+            var Account = await context.Balances.Where(e => e.UserId == userId).FirstOrDefaultAsync();
+
+            Account.Amount = Account.Amount - amount;
+            await context.SaveChangesAsync();
+        }
+
+        public static async void AddBalance(BankDbContext context, decimal amount, string type, string email)
+        {
+            var userId = await context.Users.Where(e => e.email == email).Select(e => e.id).FirstOrDefaultAsync();
+            var Account = await context.Balances.Where(e => e.UserId == userId).FirstOrDefaultAsync();
+
+            Account.Amount = Account.Amount + amount;
+            await context.SaveChangesAsync();
+        }
+
+        public static async void UserTransfer(BankDbContext context, decimal amount, string type, string email, string id)
+        {
+            var userId = await context.Users.Where(e => e.email == email).Select(e => e.id).FirstOrDefaultAsync();
+            var ReciverId = await context.Users.Where(e => e.email == id).Select(e => e.id).FirstOrDefaultAsync();
+
+            var Sender = await context.Balances.Where(e => e.UserId == userId).FirstOrDefaultAsync();
+            var Reciver = await context.Balances.Where(e => e.UserId == userId).FirstOrDefaultAsync();
+
+            Sender.Amount = Sender.Amount - amount;
+            Reciver.Amount = Reciver.Amount + amount;
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
