f9653b70f52665f71d627c28f3153662dea7069f
abhi6004 <abhiradadiya30012004@gmail.com>
2025-10-03 23:10:57 +0530

    New class added for the Loan and Customer section

--- BankEntities/Users.cs
+++ BankEntities/Users.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SecureBank_Pro.BankEntities
 {
@@ -29,6 +30,9 @@ namespace SecureBank_Pro.BankEntities
         public bool is_active { get; set; } = false;
         public DateTime? created_at { get; set; }
 
+        public Balance? Balance { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = new HashSet<Transaction>();
+
         public Users(Users other)
         {
             this.id = other.id;
@@ -57,6 +61,9 @@ namespace SecureBank_Pro.BankEntities
     {
         [Key]
         public int UserId { get; set; } // PK + FK
+            
+        [ForeignKey("UserId")]
+        public Users User { get; set; } = null!;
         public decimal Amount { get; set; }
         public string CurrencyCode { get; set; }
         public DateTime LastUpdated { get; set; }
@@ -79,6 +86,10 @@ namespace SecureBank_Pro.BankEntities
         [Key]
         public long Trasactionsid { get; set; } // PK
         public int UserId { get; set; } // FK
+
+        [ForeignKey("UserId")]
+        public Users User { get; set; } = null!;
+
         public string TransactionType { get; set; }
         public decimal Amount { get; set; }
         public decimal BalanceAfter { get; set; }
@@ -122,4 +133,133 @@ namespace SecureBank_Pro.BankEntities
 
         public ChatHistory() { }
     }
+
+    public class Application
+    {
+        [Key]
+        public int ApplicationId { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }       // FK to Users
+        public Users Customer { get; set; } = null!;
+
+        [ForeignKey("CustomerOffer")]
+        public int OfferId { get; set; }          // FK to CustomerOffers
+        public CustomerOffer CustomerOffer { get; set; } = null!;
+
+        public DateTime ApplicationDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string? Notes { get; set; }
+
+        public Application() { }
+
+        public Application(Application other)
+        {
+            this.ApplicationId = other.ApplicationId;
+            this.CustomerId = other.CustomerId;
+            this.OfferId = other.OfferId;
+            this.ApplicationDate = other.ApplicationDate;
+            this.Status = other.Status;
+            this.Notes = other.Notes;
+        }
+
+    }
+
+    public class ActiveLoan
+    {
+        [Key]
+        public int LoanId { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }       // FK to Users
+        public Users Customer { get; set; } = null!;
+
+        public DateTime LoanStartDate { get; set; }
+        public decimal LoanAmount { get; set; }
+        public decimal InterestRate { get; set; }
+
+        public int TotalInstallments { get; set; }
+        public decimal InstallmentAmount { get; set; }
+        public decimal? PenaltyPercent { get; set; }
+
+        public string LoanStatus { get; set; } = string.Empty;
+        public DateTime? DueDate { get; set; }
+
+        public ActiveLoan() { }
+
+        public ActiveLoan(ActiveLoan other)
+        {
+            this.LoanId = other.LoanId;
+            this.CustomerId = other.CustomerId;
+            this.LoanStartDate = other.LoanStartDate;
+            this.LoanAmount = other.LoanAmount;
+            this.InterestRate = other.InterestRate;
+            this.TotalInstallments = other.TotalInstallments;
+            this.InstallmentAmount = other.InstallmentAmount;
+            this.PenaltyPercent = other.PenaltyPercent;
+            this.LoanStatus = other.LoanStatus;
+            this.DueDate = other.DueDate;
+        }
+
+    }
+
+    public class CustomerOffer
+    {
+        [Key]
+        public int CustomerOfferId { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Users Customer { get; set; } = null!;
+
+        public decimal LoanAmount { get; set; }
+        public decimal? CustomInterestRate { get; set; }
+        public DateTime? CustomValidTill { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public CustomerOffer() { }
+
+        public CustomerOffer(CustomerOffer other)
+        {
+            this.CustomerOfferId = other.CustomerOfferId;
+            this.CustomerId = other.CustomerId;
+            this.LoanAmount = other.LoanAmount;
+            this.CustomInterestRate = other.CustomInterestRate;
+            this.CustomValidTill = other.CustomValidTill;
+            this.CreatedOn = other.CreatedOn;
+        }
+
+    }
+
+    public class Offer
+    {
+        [Key]
+        public int OfferId { get; set; }
+
+        public string OfferType { get; set; } = string.Empty;
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal? InterestRate { get; set; }
+
+        public DateTime? ValidTill { get; set; }
+
+        // Parameterless Constructor (Empty Constructor)
+        public Offer() { }
+
+        // Copy Constructor
+        public Offer(Offer other)
+        {
+            this.OfferId = other.OfferId;
+            this.OfferType = other.OfferType;
+            this.Title = other.Title;
+            this.Description = other.Description;
+            this.InterestRate = other.InterestRate;
+            this.ValidTill = other.ValidTill;
+        }
+    }
+
+
 }
