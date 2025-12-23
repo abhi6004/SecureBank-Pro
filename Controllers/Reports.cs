ec49dee02686ae81a3f43af9be17240c6ffdc947
abhi6004 <abhiradadiya30012004@gmail.com>
2025-12-23 19:22:51 +0530

    Reports.cs

---
+++ Controllers/Reports.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecureBank_Pro.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Transaction()
+        {
+            return View();
+        }
+
+        public IActionResult EMI()
+        {
+            return View();
+        }
+    }
+}
